a16966e6ce83af4fff9ee9188105c894dca51bb3
DoaaSalmen <doaasalmen258@gmail.com>
2026-09-15 18:58:17 +0200

    create temporarily database

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 1500,
+        category: "Electronics",
+        stock: 12
+    },
+    {
+        id: 2,
+        name: "Keyboard",
+        price: 120,
+        category: "Electronics",
+        stock: 20
+    },
+    {
+        id: 3,
+        name: "Office Chair",
+        price: 320,
+        category: "Furniture",
+        stock: 15
+    }
+];
+
+module.exports = products;
